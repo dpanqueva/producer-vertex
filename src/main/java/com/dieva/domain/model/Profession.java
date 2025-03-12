0a78b29731fcbac5c7124c9924d582298f553ac4
Diego Panqueva <diego.panqueva@glinttglobal.com>
2025-03-12 16:38:32 +0100

    Feat: include object profession for sent to kafka

---
+++ src/main/java/com/dieva/domain/model/Profession.java
@@ -0,0 +1,54 @@
+package com.dieva.domain.model;
+
+import java.io.Serializable;
+
+public class Profession implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private String description;
+    private String code;
+
+    public Profession() {
+    }
+
+    public Profession(Long id, String name, String description, String code) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.code = code;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
